996dcb22c173fb143820c4a1a803e003426a03b8
JSDeve <shavkat@swiftrelay.io>
2022-09-03 23:40:41 +0500

    card title changed to title2

--- src/components/Phones/Phones.js
+++ src/components/Phones/Phones.js
@@ -3,13 +3,16 @@ import React from 'react';
 function Phones(props) {
     return (
         <div>
+            <div className='Search' >
+                
+            </div>
             <div className='container mt-3'>
             <div className='row gy-3 '>
                 <div className='col-md-3 me-20px'>
                   <div className="card">
                     <img src="https://m-cdn.phonearena.com/images/reviews/244685-image/14-Pro-MAX-PURPLE.jpg" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">Card title2</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
